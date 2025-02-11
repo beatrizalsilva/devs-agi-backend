af5388bc0c1c53c967051359ac48ad1e0ba6eac5
Beatriz Alves <alvesbeatriz148@gmail.com>
2025-02-11 16:01:59 -0300

    feat: cálculo da trajetória de um projétil

---
+++ src/devsagi/semana02/maratona/Trajetoria.java
@@ -0,0 +1,26 @@
+package devsagi.semana02.maratona;
+
+import java.util.Scanner;
+
+public class Trajetoria {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double anguloGraus, velocidade, x, y, t = 0.01, g = 9.81, anguloRadianos;
+
+        System.out.print("Digite a velocidade inicial (m/s): ");
+        velocidade = input.nextDouble();
+        System.out.print("Digite o ângulo de inclinação (graus): ");
+        anguloGraus = input.nextDouble();
+        input.close();
+
+        anguloRadianos = Math.toRadians(anguloGraus);
+
+        do {
+            x = velocidade * Math.cos(anguloRadianos) * t;
+            y = (velocidade * Math.sin(anguloRadianos) * t) - (0.5 * g * t * t);
+            t += 0.01;
+
+            System.out.printf("(x = %.2f, y =%.2f)\n", x, y);
+        } while (y >= 0);
+    }
+}
