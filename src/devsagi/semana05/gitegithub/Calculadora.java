7bd934582bd1949db5f254b390270b43b48837bf
beatriz.santos <alvesbeatriz148@gmail.com>
2025-02-18 09:45:21 -0300

    feat: adiciona teste da calculadora para aula de Git e GitHub

---
+++ src/devsagi/semana05/gitegithub/Calculadora.java
@@ -0,0 +1,11 @@
+package com.agibank.semana05.gitegithub;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        System.out.println("Resultado: " + soma(5,6));
+    }
+
+    public static int soma(int a, int b) {
+        return a + b;
+    }
+}
